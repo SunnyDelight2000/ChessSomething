a0fb5bc9b7ad8650cd8c5585b2cb0f6a22b1859f
SunnyDelight2000 <sandrinelecomt@hotmail.com>
2024-04-30 17:37:05 +0100

    Start of matrix
    Removed bee, created row size and column to create board!

--- main.cpp
+++ main.cpp
@@ -8,9 +8,34 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout<<"Hello World";
-
+    int ROW_SIZE = 8;
+    int COLUMN_SIZE = 8;
+    
+    int board[ROW_SIZE][COLUMN_SIZE] = { 
+    { 1, 2, 3, 4, 5, 6, 7, 8 },
+    { 1, 2, 3, 4, 5, 6, 7, 8 },
+    { 1, 2, 3, 4, 5, 6, 7, 8 },
+    { 1, 2, 3, 4, 5, 6, 7, 8 },
+    { 1, 2, 3, 4, 5, 6, 7, 8 },
+    { 1, 2, 3, 4, 5, 6, 7, 8 },
+    { 1, 2, 3, 4, 5, 6, 7, 8 },
+    { 1, 2, 3, 4, 5, 6, 7, 8 },
+    };
+    
+    for (int index = 0; index < ROW_SIZE; index++) {
+        
+        for (int something = 0; something < COLUMN_SIZE; something++) {
+            cout << board[index][something];
+        }
+        
+        cout << endl;
+        
+    }
+    
+    
     return 0;
 }
